d4bb99cbabc0161b40ad40d061a71485eb83df67
xStarBlade <36200500+xStarBlade@users.noreply.github.com>
2024-10-21 03:48:25 -0500

    Update 2024-10-21.js

--- src/js/data/2024-06-10.js
+++ src/js/data/2024-06-10.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2024-06-11"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2024-10-21"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
