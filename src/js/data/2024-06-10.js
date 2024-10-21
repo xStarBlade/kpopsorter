2d231cd74f731c5c8e6ab79e016f252b062604ff
xStarBlade <36200500+xStarBlade@users.noreply.github.com>
2024-10-21 02:55:33 -0500

    Update 2024-06-10.js

--- src/js/data/2024-06-10.js
+++ src/js/data/2024-06-10.js
@@ -4123,7 +4123,7 @@ dataSet[dataSetVersion].characterData = [
     },
     {
         name: "tripleS Kim Yooyeon",
-        img: "UzUIDb9.png",
+        img: "https://imgur.com/5LWT9r4.jpeg",
         opts: {
             group: ["tripleS"],
             gen: ["gen4"]
