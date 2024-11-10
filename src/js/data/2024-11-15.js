8b7d7f3459c25280b61769a1e55ee74bfca631aa
xStarBlade <36200500+xStarBlade@users.noreply.github.com>
2024-11-10 09:53:25 -0600

    Update and rename 2024-06-10.js to 2024-11-15.js

--- src/js/data/2024-11-15.js
+++ src/js/data/2024-11-15.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2024-10-21"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2024-11-15"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
