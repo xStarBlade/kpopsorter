04f53db600013ba2ed869bba00000270311c79fc
xStarBlade <36200500+xStarBlade@users.noreply.github.com>
2025-05-03 15:34:19 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -657,10 +657,10 @@ function setLatestDataset() {
 /** Populate option list. */
 function populateOptions() {
   const optList = document.querySelector('.options');
-  const optInsert = (name, id, tooltip, checked = true, disabled = false) => {
+  const optInsert = (name, id, tooltip, checked = true, disabled = true) => {
     return `<div><label title="${tooltip?tooltip:name}"><input id="cb-${id}" type="checkbox" ${checked?'checked':''} ${disabled?'disabled':''}> ${name}</label></div>`;
   };
-  const optInsertLarge = (name, id, tooltip, checked = false) => {
+  const optInsertLarge = (name, id, tooltip, checked = true) => {
     return `<div class="large option"><label title="${tooltip?tooltip:name}"><input id="cbgroup-${id}" type="checkbox" ${checked?'checked':''}> ${name}</label><div id="select-all-container"><a id="select-all" style="display: none;"></a></div></div>`;
   };
 
