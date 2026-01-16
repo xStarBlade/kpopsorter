433b2b80e565b7ffd47bd2ee9e449b95e5b5a45f
KoreabooCE <36200500+KoreabooCE@users.noreply.github.com>
2026-01-16 02:29:53 -0600

    Update 2025-11-01.js

--- src/js/data/2025-11-01.js
+++ src/js/data/2025-11-01.js
@@ -4422,7 +4422,7 @@ dataSet[dataSetVersion].characterData = [
     },
     {
         name: "WOOAH Minseo",
-        img: "EFoSZXs.jpeg",
+        img: "l0oXpqR.jpeg",
         opts: {
             group: ["WOOAH"],
             gen: ["gen4"]
@@ -7627,6 +7627,7 @@ dataSet[dataSetVersion].characterData = [
         }
     },
 ];
+
 
 
 
